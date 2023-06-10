89331116d08ff1d4c1a786e1f48f5ad3c192b7e0
ashwinipatilap <ashwinipatilapk18@gmail.com>
2023-06-10 12:14:47 +0530

    Employee add backend implemented

--- Employee_Management_System-backend/Employee_Management_System/src/main/java/com/emp/controller/employeeController.java
+++ Employee_Management_System-backend/Employee_Management_System/src/main/java/com/emp/controller/employeeController.java
@@ -1,10 +1,30 @@
 package com.emp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.emp.model.Employee;
+import com.emp.service.IemployeeService;
 
 @RestController
 @RequestMapping("/employee")
 public class employeeController {
+	@Autowired(required=true)
+	IemployeeService empserv;
+	
+	@PostMapping("/addEmp")
+	public ResponseEntity<Employee> addEmp(@RequestBody Employee employee ){
+		Employee newemp = empserv.addEmp(employee);
+		if(newemp != null) {
+			return ResponseEntity.ok().body(newemp);
+		}else {
+			return ResponseEntity.status(404).body(null);
+		}
+	}
+	
 
 }
